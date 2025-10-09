95024fe2563679d24b45db5518dda82dfe969622
Pavel D <pvd367@gmail.com>
2025-10-09 22:20:06 +0800

    Add localizations: de, es, fr, hi, js, pt, zh; update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class AppConst {
   static const String appName = 'SMS Telebot';
-  static const String appVersion = '0.3.2';
+  static const String appVersion = '0.3.3';
   static const filterKeys = ['wSenders', 'wSms', 'bSenders', 'bSms'];
 }
