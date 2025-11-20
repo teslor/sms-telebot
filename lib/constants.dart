c71f9587b2cfe6ce9f7e42805c90306aa7274b3e
Pavel D <pvd367@gmail.com>
2025-11-20 20:25:34 +0800

    Update version

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class AppConst {
   static const String appName = 'SMS Telebot';
-  static const String appVersion = '0.3.3';
+  static const String appVersion = '0.4.0';
   static const filterKeys = ['wSenders', 'wSms', 'bSenders', 'bSms'];
 }
