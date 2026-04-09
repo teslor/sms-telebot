78f6ae52c000b17ebe8f4d516458c21a140e3c01
Pavel D <pvd367@gmail.com>
2026-04-09 23:03:29 +0800

    Update version info

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 class AppConst {
   static const String appName = 'SMS Telebot';
-  static const String appVersion = '0.5.1';
+  static const String appVersion = '0.6.0';
   static const String mainChannel = 'sms_telebot/main';
   static const filterKeys = ['whitelistSenders', 'whitelistBody', 'blacklistSenders', 'blacklistBody'];
 }
