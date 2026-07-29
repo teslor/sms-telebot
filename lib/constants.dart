c81fb7bf5facd3a4c9019b37308929c2c5c7f0d1
Pavel D <pvd367@gmail.com>
2026-07-29 20:05:59 +0800

    Update version; remove backup rules

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 class AppConst {
   static const String appName = 'SMS Telebot';
   static const String appLink = 'https://github.com/teslor/sms-telebot';
-  static const String appVersion = '0.7.2';
+  static const String appVersion = '0.8.0';
   static const String mainChannel = 'sms_telebot/main';
   static const filterKeys = ['whitelistSenders', 'whitelistBody', 'blacklistSenders', 'blacklistBody'];
 }
